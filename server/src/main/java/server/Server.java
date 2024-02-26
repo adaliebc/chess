752605a1d0e0913a95fcdaef3f2c38f86b14ab21
Adalie Crockett <adaliebc@byu.edu>
2024-02-26 14:47:38 -0700

    implementing static file and service folder

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,14 @@
 package server;
 
+import com.google.gson.Gson;
 import spark.*;
+import java.util.*;
+import service.*;
 
 public class Server {
+    public static void main(String[] args) {
+        new Server().run(8080);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,11 +16,24 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.get("/hello", (req, resp) -> "Hello World");
+        Spark.delete("/db", this::clearBody);
+        Spark.post("/user", this::userBody);
         Spark.awaitInitialization();
         return Spark.port();
     }
-
+    private Object clearBody(Request req, Response res) /*throws ResponseException*/ {
+        ChessService service = new ChessService.clearService();
+        res.status(204);
+        return "";
+    }
+    private Object userBody(Request req, Response res) {
+        var body = new Gson().toJson(Map.of("success", true));
+        res.type("application/json");
+        res.status(200);
+        res.body(body);
+        return body;
+    }
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
---
+++ server/src/main/java/service/ChessService.java
@@ -0,0 +1,32 @@
+package service;
+
+import java.util.Collection;
+
+public class ChessService {
+
+    private final DataAccess dataAccess;
+
+    /*public PetService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public Pet addPet(Pet pet) throws ResponseException {
+        return dataAccess.addPet(pet);
+    }
+
+    public Collection<Pet> listPets() throws ResponseException {
+        return dataAccess.listPets();
+    }
+
+    public Pet getPet(int id) throws ResponseException {
+        return dataAccess.getPet(id);
+    }
+
+    public void deletePet(Integer id) throws ResponseException {
+        dataAccess.deletePet(id);
+    }*/
+
+    public void clearService() /*throws ResponseException*/ {
+        //dataAccess.clearService();
+    }
+}
