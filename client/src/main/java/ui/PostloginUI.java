6e14d8602822db3fa8ccffed64e5a08921944b67
Adalie Crockett <adaliebc@byu.edu>
2024-03-19 15:14:20 -0600

    removing files tp consolidate method

--- client/src/main/java/ui/PostloginUI.java
+++ client/src/main/java/ui/PostloginUI.java
@@ -18,4 +18,21 @@ public class PostloginUI {
     //esle
     //print out command unknown, try again, maybe the help screen
 
+
+    //class where we create and deal with games
+
+    //create game
+    //take input and send it to server
+    //receives gameID and uses chessgame
+    //sends to gameplayUI
+
+    //join game
+    // if the input list doesn't have info for joining as a player, fill in the blanks with null
+    //if success
+    //send to gameplayui
+
+    //list game
+    //sends link to server
+    //prints out list of games
+
 }
