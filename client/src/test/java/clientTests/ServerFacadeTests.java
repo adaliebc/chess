668283f6321a55fa5aec0f7c1ec55f05162f2a93
Adalie Crockett <adaliebc@byu.edu>
2024-03-20 23:49:09 -0600

    attemptin to implement list tests

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -37,44 +37,51 @@ public class ServerFacadeTests {
     }
 
     @Test
+    @Order(1)
     public void registerUserPositiveTest() {
         String[] list = new String[] {"register", "username", "password", "email"};
         Assertions.assertTrue(facade.register(list));
     }
 
     @Test
+    @Order(2)
     public void registerUserNegativeTest() {
         String[] list = new String[] {"register", "username", "password"};
         Assertions.assertFalse(facade.register(list));
     }
 
     @Test
+    @Order(3)
     public void loginUserPositiveTest() {
         String[] list = new String[] {"login", "username", "password"};
         Assertions.assertTrue(facade.login(list));
     }
 
     @Test
+    @Order(4)
     public void loginUserNegativeTest() {
         String[] list = new String[] {"login", "username"};
         Assertions.assertFalse(facade.login(list));
     }
 
     @Test
+    @Order(5)
     public void createGamePositiveTest() {
         String[] list = new String[] {"login", "username", "password"};
-        Assertions.assertTrue(facade.login(list));
+        facade.login(list);
         list = new String[] {"create", "name"};
         Assertions.assertTrue(facade.createGame(list) != 0);
     }
 
     @Test
+    @Order(6)
     public void createGameNegativeTest() {
         String[] list = new String[] {"create", "username", "password"};
         Assertions.assertEquals(0, facade.createGame(list));
     }
 
     @Test
+    @Order(7)
     public void joinGamePositiveTest() {
         String[] list = new String[] {"login", "username", "password"};
         Assertions.assertTrue(facade.login(list));
@@ -86,6 +93,7 @@ public class ServerFacadeTests {
     }
 
     @Test
+    @Order(8)
     public void joinGameNegativeTest() {
         String[] list = new String[] {"login", "username", "password"};
         Assertions.assertTrue(facade.login(list));
@@ -94,6 +102,30 @@ public class ServerFacadeTests {
         String gameID = Integer.toString(gameIDint);
         list = new String[] {"observe", gameID, "pink"};
         Assertions.assertNull(facade.joinGame(list));
+    }
+
+    @Test
+    @Order(9)
+    public void listGamePositiveTest() {
+        String[] list = new String[] {"login", "username", "password"};
+        facade.login(list);
+        list = new String[] {"create", "name"};
+        facade.createGame(list);
+        Assertions.assertTrue(facade.listGames());
+    }
+
+    @Test
+    @Order(10)
+    public void listGameNegativeTest() {
+        String[] list = new String[] {"login", "username", "password"};
+        Assertions.assertTrue(facade.login(list));
+        list = new String[] {"create", "name"};
+        int gameIDint = facade.createGame(list);
+        String gameID = Integer.toString(gameIDint);
+        list = new String[] {"observe", gameID};
+        facade.joinGame(list);
+        facade.logout();
+        Assertions.assertFalse(facade.listGames());
     }
 
 }
