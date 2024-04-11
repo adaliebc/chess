c99fa5e735223e7b3d95749de2deb3c15e4592a8
adaliebc <119454582+adaliebc@users.noreply.github.com>
2024-04-10 19:21:13 -0600

    continuing to psudo code and beginning to implement connection and ws handler

--- server/src/main/java/websocket/Connection.java
+++ server/src/main/java/websocket/Connection.java
@@ -1,7 +1,34 @@
 package websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+//import webSocketMessages.Notification;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Connection {
+    String username;
+    Session session;
+    //may need to change Session in connections to Connection
+    public final ConcurrentHashMap<String, Session> connections = new ConcurrentHashMap<>();
+
+    //consructor
+    public Connection(String username, Session session){
+        this.username = username;
+        this.session = session;
+        connections.put(username, session);
+    }
     //create a connection
     //stops connection (called by handler)
+    public void remove(String username){
+        connections.remove(username);
+    }
+
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
     //broadcast message to all connections
 }
--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,6 +1,33 @@
 package websocket;
 
+import com.google.gson.Gson;
+//import dataaccess.DataAccess;
+//import exception.ResponseException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+//import webSocketMessages.Action;
+//import webSocketMessages.Notification;
+
+import java.io.IOException;
+import java.util.Timer;
+
 public class WebSocketHandler {
+    //Connection connection = new Connection();
+    //establish a link to connections in connections
+
     //manage connections
+
+    //add a connection
+    // create a new connection
+    //send a message to everyone saying this guy joined
+    //call broadcast function to do that
+
+    //remove a connection
+    //remove it from connections list and send message
+
     //send messages
+
+    //make a move message
+    //send out a message that says username moved here
 }
